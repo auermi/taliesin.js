03cc69b9fdbe7ec56a4def3d7f542c2b529530a4
--global <michael@michael-auer.co>
2015-08-06 01:46:28 -0400

    Add outer width/height and fixed width/height, innerWidth/innerHeight issues

--- rabbak.js
+++ rabbak.js
@@ -15,16 +15,62 @@ function rabbak(selector) {
 }
 
 ///////// FUNCTIONS /////////
+// OBJECT OUTER WIDTH
+rabbak.prototype.outerWidth = function(includeMargin) {
+  if (typeOf(this.target) === "htmlcollection") {
+    var widths = [];
+    for (i = 0; i < this.target.length; i++) {
+      if (includeMargin == null || false) {
+        widths.push(this.target[i].getBoundingClientRect().width);
+      } else if (includeMargin) {
+        var elementHorizontalMargin = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('margin-left')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('margin-right'));
+        widths.push(this.target[i].getBoundingClientRect().width + elementHorizontalMargin);
+      }
+    }
+    return widths;
+  } else {
+    if (includeMargin == null || false) {
+      return this.target.getBoundingClientRect().width;
+    } else if (includeMargin) {
+      var elementHorizontalMargin = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('margin-left')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('margin-right'));
+      return this.target.getBoundingClientRect().width + elementHorizontalMargin;
+    }
+  }
+}
+// OBJECT OUTER HEIGHT
+rabbak.prototype.outerHeight = function(includeMargin) {
+  if (typeOf(this.target) === "htmlcollection") {
+    var heights = [];
+    for (i = 0; i < this.target.length; i++) {
+      if (includeMargin == null || false) {
+        heights.push(this.target[i].getBoundingClientRect().height);
+      } else if (includeMargin) {
+        var elementVerticalMargin = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('margin-top')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('margin-bottom'));
+        heights.push(this.target[i].getBoundingClientRect().height + elementVerticalMargin);
+      }
+    }
+    return heights;
+  } else {
+    if (includeMargin == null || false) {
+      return this.target.getBoundingClientRect().height;
+    } else if (includeMargin) {
+      var elementVerticalMargin = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('margin-top')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('margin-bottom'));
+      return this.target.getBoundingClientRect().height + elementVerticalMargin;
+    }
+  }
+}
 // OBJECT INNER WIDTH
 rabbak.prototype.innerWidth = function() {
   if (typeOf(this.target) === "htmlcollection") {
     var widths = [];
     for (i = 0; i < this.target.length; i++) {
-      widths.push(this.target[i].offsetWidth);
+      var elementHorizontalBorder = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-left')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-right'));
+      widths.push(this.target[i].getBoundingClientRect().width - elementHorizontalBorder);
     }
     return widths;
   } else {
-    return this.target.offsetWidth;
+    var elementHorizontalBorder = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-left')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-right'));
+    return this.target.offsetWidth - elementHorizontalBorder;
   }
 }
 // OBJECT INNER HEIGHT
@@ -32,37 +78,45 @@ rabbak.prototype.innerHeight = function() {
   if (typeOf(this.target) === "htmlcollection") {
     var heights = [];
     for (i = 0; i < this.target.length; i++) {
-      heights.push(this.target[i].offsetHeight);
+      var elementVerticalBorder = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-top')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-bottom'));
+      heights.push(this.target[i].getBoundingClientRect().height - elementVerticalBorder);
     }
     return heights;
   } else {
-    return this.target.offsetHeight;
+    var elementVerticalBorder = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-top')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-bottom'));
+    return this.target.offsetHeight - elementVerticalBorder;
   }
 }
 // OBJECT WIDTH
 rabbak.prototype.width = function() {
-  var elementHorizontalPadding = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-left')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-right'));
   if (typeOf(this.target) === "htmlcollection") {
     var widths = [];
     for (i = 0; i < this.target.length; i++) {
-      widths.push(this.target[i].getBoundingClientRect().width - elementHorizontalPadding);
+      var elementHorizontalPadding = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('padding-left')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('padding-right'));
+      var elementHorizontalBorder = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-left')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-right'));
+      widths.push(this.target[i].getBoundingClientRect().width - elementHorizontalPadding - elementHorizontalBorder);
     }
     return widths;
   } else {
-    return this.target.getBoundingClientRect().width - elementHorizontalPadding;
+    var elementHorizontalPadding = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-left')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-right'));
+    var elementHorizontalBorder = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-left')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-right'));
+    return this.target.getBoundingClientRect().width - elementHorizontalPadding - elementHorizontalBorder;
   }
 }
 // OBJECT HEIGHT
 rabbak.prototype.height = function() {
-  var elementVerticalPadding = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-top')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-bottom'));
   if (typeOf(this.target) === "htmlcollection") {
     var heights = [];
     for (i = 0; i < this.target.length; i++) {
-      heights.push(this.target[i].getBoundingClientRect().height - elementVerticalPadding);
+      var elementVerticalPadding = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('padding-top')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('padding-bottom'));
+      var elementVerticalBorder = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-top')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-bottom'));
+      heights.push(this.target[i].getBoundingClientRect().height - elementVerticalPadding - elementVerticalBorder);
     }
     return heights;
   } else {
-    return this.target.getBoundingClientRect().height - elementVerticalPadding;
+    var elementVerticalPadding = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-top')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-bottom'));
+    var elementVerticalBorder = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-top')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-bottom'));
+    return this.target.getBoundingClientRect().height - elementVerticalPadding - elementVerticalBorder;
   }
 }
 // ADD CLASS
@@ -75,6 +129,3 @@ rabbak.prototype.addClass = function(className) {
   }
   this.target.classList.add(className);
 }
-
-// ALAKAZAM
-var $ = new rabbak("#box");
