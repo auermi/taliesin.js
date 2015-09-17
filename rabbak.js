1451e6c264f4c2acddbc3ac9b478eb5ca8712e6f
--global <michael@michael-auer.co>
2015-09-16 22:22:50 -0400

    Restructured to be callable like a library

--- rabbak.js
+++ rabbak.js
@@ -1,132 +1,158 @@
 (function () {
-  // GENIUS -> https://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/
-  var typeOf = function (obj) {
-    return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
-  }
-  // SET-UP
-  function rabbak (selector) {
-    if (selector.charAt(0) === '#') {
-      selector = selector.slice(1, selector.length);
-      this.target = document.getElementById(selector);
-    } else if (selector.charAt(0) === '.') {
-      selector = selector.slice(1, selector.length);
-      this.target = document.getElementsByClassName(selector);
-    }
-  }
-
-  // FUNCTIONS
-  // OBJECT OUTER WIDTH
-  rabbak.prototype.outerWidth = function(includeMargin) {
-    if (typeOf(this.target) === 'htmlcollection') {
-      var widths = [];
-      for (var i = 0; i < this.target.length; i++) {
-        if (includeMargin == null || false) {
-          widths.push(this.target[i].getBoundingClientRect().width);
-        } else if (includeMargin) {
-          var elementHorizontalMargin = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('margin-left')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('margin-right'));
-          widths.push(this.target[i].getBoundingClientRect().width + elementHorizontalMargin);
+    'use strict';
+    /*jslint browser: true*/
+    /*jslint vars: true, plusplus: true, devel: true, indent: 4, todo: true */
+    function T(id) {
+        if (id) {
+            // Avoid clobbering the window scope:
+            // return a new T object if we're in the wrong scope
+            if (window === this) {
+                return new T(id);
+            }
+            // We're in the correct object scope:
+            // Init our element object and return the object
+            this.e = document.getElementById(id);
+            console.log('Error. No ID Specified');
+            return this;
         }
-      }
-      return widths;
-    } else {
-      if (includeMargin == null || false) {
-        return this.target.getBoundingClientRect().width;
-      } else if (includeMargin) {
-        var elementHorizontalMargin = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('margin-left')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('margin-right'));
-        return this.target.getBoundingClientRect().width + elementHorizontalMargin;
-      }
+        return console.log('Error. Can\'t find selector of called ' + id);
     }
-  }
-  // OBJECT OUTER HEIGHT
-  rabbak.prototype.outerHeight = function (includeMargin) {
-    if (typeOf(this.target) === 'htmlcollection') {
-      var heights = [];
-      for (var i = 0; i < this.target.length; i++) {
-        if (includeMargin == null || false) {
-          heights.push(this.target[i].getBoundingClientRect().height);
-        } else if (includeMargin) {
-          var elementVerticalMargin = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('margin-top')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('margin-bottom'));
-          heights.push(this.target[i].getBoundingClientRect().height + elementVerticalMargin);
+    T.prototype = {
+        // https://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/
+        typeOf: function (object) {
+            return ({}).toString.call(object).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+        },
+        // Object Outer Width
+        outerWidth: function (includeMargin) {
+            var elementHorizontalMargin;
+            if (this.typeOf(this.target) === 'htmlcollection') {
+                var widths = [];
+                var i;
+                for (i = 0; i < this.target.length; i++) {
+                    if (includeMargin === null || false) {
+                        widths.push(this.target[i].getBoundingClientRect().width);
+                    } else if (includeMargin) {
+                        elementHorizontalMargin = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('margin-left')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('margin-right'));
+                        widths.push(this.target[i].getBoundingClientRect().width + elementHorizontalMargin);
+                    }
+                }
+                return widths;
+            }
+            if (includeMargin === null || false) {
+                return this.target.getBoundingClientRect().width;
+            }
+            elementHorizontalMargin = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('margin-left')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('margin-right'));
+            return this.target.getBoundingClientRect().width + elementHorizontalMargin;
+        },
+        outerHeight: function (includeMargin) {
+            var elementVerticalMargin;
+            var i;
+            if (this.typeOf(this.target) === 'htmlcollection') {
+                var heights = [];
+                for (i = 0; i < this.target.length; i++) {
+                    if (includeMargin === null || false) {
+                        heights.push(this.target[i].getBoundingClientRect().height);
+                    } else if (includeMargin) {
+                        elementVerticalMargin = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('margin-top')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('margin-bottom'));
+                        heights.push(this.target[i].getBoundingClientRect().height + elementVerticalMargin);
+                    }
+                }
+                return heights;
+            }
+            if (includeMargin === null || false) {
+                return this.target.getBoundingClientRect().height;
+            }
+            elementVerticalMargin = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('margin-top')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('margin-bottom'));
+            return this.target.getBoundingClientRect().height + elementVerticalMargin;
+        },
+        // OBJECT INNER WIDTH
+        innerWidth: function () {
+            var elementHorizontalBorder;
+            if (this.typeOf(this.target) === 'htmlcollection') {
+                var widths = [];
+                var i;
+                for (i = 0; i < this.target.length; i++) {
+                    elementHorizontalBorder = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-left')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-right'));
+                    widths.push(this.target[i].getBoundingClientRect().width - elementHorizontalBorder);
+                }
+                return widths;
+            }
+            elementHorizontalBorder = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-left')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-right'));
+            return this.target.offsetWidth - elementHorizontalBorder;
+        },
+        // OBJECT INNER HEIGHT
+        innerHeight: function () {
+            var elementVerticalBorder;
+            if (this.typeOf(this.target) === 'htmlcollection') {
+                var heights = [];
+                var i;
+                for (i = 0; i < this.target.length; i++) {
+                    elementVerticalBorder = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-top')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-bottom'));
+                    heights.push(this.target[i].getBoundingClientRect().height - elementVerticalBorder);
+                }
+                return heights;
+            }
+            elementVerticalBorder = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-top')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-bottom'));
+            return this.target.offsetHeight - elementVerticalBorder;
+        },
+        // OBJECT WIDTH
+        width: function () {
+            var elementHorizontalPadding;
+            var elementHorizontalBorder;
+            if (this.typeOf(this.target) === 'htmlcollection') {
+                var widths = [];
+                var i;
+                for (i = 0; i < this.target.length; i++) {
+                    elementHorizontalPadding = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('padding-left')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('padding-right'));
+                    elementHorizontalBorder = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-left')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-right'));
+                    widths.push(this.target[i].getBoundingClientRect().width - elementHorizontalPadding - elementHorizontalBorder);
+                }
+                return widths;
+            }
+            elementHorizontalPadding = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-left')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-right'));
+            elementHorizontalBorder = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-left')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-right'));
+            return this.target.getBoundingClientRect().width - elementHorizontalPadding - elementHorizontalBorder;
+        },
+        // OBJECT HEIGHT
+        height: function () {
+            var elementVerticalPadding;
+            var elementVerticalBorder;
+            if (this.typeOf(this.target) === 'htmlcollection') {
+                var heights = [];
+                var i;
+                for (i = 0; i < this.target.length; i++) {
+                    elementVerticalPadding = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('padding-top')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('padding-bottom'));
+                    elementVerticalBorder = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-top')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-bottom'));
+                    heights.push(this.target[i].getBoundingClientRect().height - elementVerticalPadding - elementVerticalBorder);
+                }
+                return heights;
+            }
+            elementVerticalPadding = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-top')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-bottom'));
+            elementVerticalBorder = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-top')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-bottom'));
+            return this.target.getBoundingClientRect().height - elementVerticalPadding - elementVerticalBorder;
+        },
+        // ADD CLASS
+        addClass: function (className) {
+            if (this.typeOf(this.target) === 'htmlcollection') {
+                var i;
+                for (i = 0; i < this.target.length; i++) {
+                    this.target[i].classList.add(className);
+                }
+                return;
+            }
+            this.target.classList.add(className);
         }
-      }
-      return heights;
-    } else {
-      if (includeMargin == null || false) {
-        return this.target.getBoundingClientRect().height;
-      } else if (includeMargin) {
-        var elementVerticalMargin = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('margin-top')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('margin-bottom'));
-        return this.target.getBoundingClientRect().height + elementVerticalMargin;
+    };
+    // TODO -> Implement old selector functionality into new library structure.
+    /*
+    function rabbak (selector) {
+      if (selector.charAt(0) === '#') {
+        selector = selector.slice(1, selector.length);
+        this.target = document.getElementById(selector);
+      } else if (selector.charAt(0) === '.') {
+        selector = selector.slice(1, selector.length);
+        this.target = document.getElementsByClassName(selector);
       }
     }
-  }
-  // OBJECT INNER WIDTH
-  rabbak.prototype.innerWidth = function() {
-    if (typeOf(this.target) === 'htmlcollection') {
-      var widths = [];
-      for (var i = 0; i < this.target.length; i++) {
-        var elementHorizontalBorder = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-left')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-right'));
-        widths.push(this.target[i].getBoundingClientRect().width - elementHorizontalBorder);
-      }
-      return widths;
-    } else {
-      var elementHorizontalBorder = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-left')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-right'));
-      return this.target.offsetWidth - elementHorizontalBorder;
-    }
-  }
-  // OBJECT INNER HEIGHT
-  rabbak.prototype.innerHeight = function() {
-    if (typeOf(this.target) === 'htmlcollection') {
-      var heights = [];
-      for (var i = 0; i < this.target.length; i++) {
-        var elementVerticalBorder = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-top')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-bottom'));
-        heights.push(this.target[i].getBoundingClientRect().height - elementVerticalBorder);
-      }
-      return heights;
-    } else {
-      var elementVerticalBorder = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-top')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-bottom'));
-      return this.target.offsetHeight - elementVerticalBorder;
-    }
-  }
-  // OBJECT WIDTH
-  rabbak.prototype.width = function() {
-    if (typeOf(this.target) === 'htmlcollection') {
-      var widths = [];
-      for (var i = 0; i < this.target.length; i++) {
-        var elementHorizontalPadding = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('padding-left')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('padding-right'));
-        var elementHorizontalBorder = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-left')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-right'));
-        widths.push(this.target[i].getBoundingClientRect().width - elementHorizontalPadding - elementHorizontalBorder);
-      }
-      return widths;
-    } else {
-      var elementHorizontalPadding = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-left')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-right'));
-      var elementHorizontalBorder = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-left')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-right'));
-      return this.target.getBoundingClientRect().width - elementHorizontalPadding - elementHorizontalBorder;
-    }
-  }
-  // OBJECT HEIGHT
-  rabbak.prototype.height = function() {
-    if (typeOf(this.target) === 'htmlcollection') {
-      var heights = [];
-      for (var i = 0; i < this.target.length; i++) {
-        var elementVerticalPadding = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('padding-top')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('padding-bottom'));
-        var elementVerticalBorder = parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-top')) + parseFloat(window.getComputedStyle(this.target[i], null).getPropertyValue('border-bottom'));
-        heights.push(this.target[i].getBoundingClientRect().height - elementVerticalPadding - elementVerticalBorder);
-      }
-      return heights;
-    } else {
-      var elementVerticalPadding = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-top')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('padding-bottom'));
-      var elementVerticalBorder = parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-top')) + parseFloat(window.getComputedStyle(this.target, null).getPropertyValue('border-bottom'));
-      return this.target.getBoundingClientRect().height - elementVerticalPadding - elementVerticalBorder;
-    }
-  }
-  // ADD CLASS
-  rabbak.prototype.addClass = function (className) {
-    if (typeOf(this.target) === 'htmlcollection') {
-      for (var i = 0; i < this.target.length; i++) {
-        this.target[i].classList.add(className)
-      }
-      return;
-    }
-    this.target.classList.add(className)
-  }
+    */
 });
